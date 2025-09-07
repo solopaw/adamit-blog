8a3674f38ce370e42860752c00b2b1feb09ad767
solopaw <adamjoelfraser@icloud.com>
2025-09-07 18:29:52 +1000

    new file:   public/android-chrome-192x192.png 	new file:   public/android-chrome-512x512.png 	new file:   public/apple-touch-icon.png 	new file:   public/favicon-16x16.png 	new file:   public/favicon-32x32.png 	new file:   public/favicon.ico 	new file:   public/site.webmanifest 	new file:   src/app/favicon.ico 	modified:   src/app/layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,21 @@
 import "@/styles/globals.css"
 import Navbar from "@/components/navbar"
 import { ThemeProvider } from "@/components/theme-provider"
+import { Metadata } from "next"
 interface RootLayoutProps {
   children: React.ReactNode
+}
+export const metadata:Metadata = {
+  title: "Adam It Page",
+  description: "The personal website of Adam It",
+
 }
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
     
       <html lang="en" suppressHydrationWarning>
         <head />
+        
         <body>
           <ThemeProvider
             attribute="class"
